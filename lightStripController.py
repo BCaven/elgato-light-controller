5f0942e7e43e3b2b4e191d21517b9deea4aef30a
Blake <blakecaven@outlook.com>
2023-11-12 19:01:47 -0500

    started work on scene management

--- lightStripController.py
+++ lightStripController.py
@@ -9,6 +9,7 @@ import socket
 from multiprocessing import Pool
 import json
 from time import sleep
+
 NUM_PORTS = 65536
 ELGATO_PORT = 9123
 
@@ -25,16 +26,49 @@ class ServiceListener:
         info = zeroconf.get_service_info(type, name)
         self.services.append(info)
 
+class Scene:
+    """
+        class to store and manipulate scenes at a high level
+    """
+    def __init__(self):
+        """
+
+        """
+        
 class LightStrip:
     """
-
+        LightStrip language
+        data: json object,  can be retrieved by making a get request to light.full_addr/elgato/lights
+            always has two keys:
+                'numberOfLights': int
+                'lights'        : list
+            each item in 'lights' is a dict that always has two keys:
+                'on'            : int
+                'brightness'    : int
+            however, the lights have two (known) modes: individual colors, and scenes
+                for individual colors, the dictionary additionally has 'hue' and 'saturation' keys (both are of type `float`)
+                for scenes:
+                    'id'                        : str
+                    'name'                      : str
+                    'numberOfSceneElements'     : int
+                    'scene'                     : list
+                each 'scene' is a list of dictionaries containing the following keys:
+                    'hue'                       : float
+                    'saturation'                : float
+                    'brightness'                : float
+                    'durationMs'                : int
+                    'transitionMs'              : int
+            when there is a 'scene', the light loops through each item in the scene
     """
     def __init__(self, addr, port, name=""):
         self.addr = addr
         self.port = port
         self.name = name
         self.full_addr = self.addr + ':' + str(self.port)
-        pass
+        print("full addr", self.full_addr)
+        self.get_strip_data() # fill in the data/info/settings of the light
+        self.get_strip_info()
+        self.get_strip_settings()
 
     def find_light_strips_zeroconf(service_type='_elg._tcp.local.', TIMEOUT=5):
         """
@@ -49,7 +83,7 @@ class LightStrip:
         except Exception:
             print("please install zeroconf to use this method")
             print("$ pip install zeroconf")
-            return
+            return []
 
         lightstrips = []
 
@@ -57,15 +91,16 @@ class LightStrip:
         zc = zeroconf.Zeroconf()
         listener = ServiceListener()
         browser = zeroconf.ServiceBrowser(zc, service_type, listener)
-        sleep(TIMEOUT) # this is not a rolling admission... I could rework it to be that way, and it might be smarter to do that
-        browser.cancel()
+        sleep(TIMEOUT)      # this is not a rolling admission... I could rework it to be that way, and it might be smarter to do that
+        browser.cancel()    # however right now this works just fine. In theory it will lose connection to the lights if they get assigned to a new IP
+                            # but in that case I am going to assume it is because the network bounces, which means this function will get called again anyways
         for service in listener.get_services():
             print("name", service.get_name())
             print("properties:", service.properties)
             print("port", service.port)
             for addr in service.addresses:
                 print("address:", socket.inet_ntoa(addr))
-            lightstrips.append(LightStrip(socket.inet_ntoa(addr), service.port, service.get_name()))
+                lightstrips.append(LightStrip(socket.inet_ntoa(addr), service.port, service.get_name()))
 
         return lightstrips
 
@@ -119,6 +154,13 @@ class LightStrip:
         """
         self.settings = requests.get('http://' + self.full_addr + '/elgato/lights/settings', verify=False).json()
         return self.settings
+    def get_strip_color(self):
+        """
+
+        """
+        light_color = self.get_strip_data()['lights'][0]
+        return (light_color['on'], light_color['hue'], light_color['saturation'], light_color['brightness'])
+
 
     def set_strip_data(self, new_data: json) -> bool:
         """
@@ -130,6 +172,25 @@ class LightStrip:
         if r.status_code == requests.codes.ok:
             self.data = r.json()
             return True
+        print(r.text)
+        return False
+    def set_strip_settings(self, new_data: json) -> bool:
+        """
+            Send a put request to update the light settings
+            Returns True on success
+        """
+        r = requests.put('http://' + self.full_addr + '/elgato/lights/settings', data=json.dumps(new_data))
+        if r.status_code == requests.codes.ok:
+            self.settings = new_data
+            return True
+        print(r.text)
+        return False
+    def set_strip_info(self, new_data: json) -> bool:
+        r = requests.put('http://' + self.full_addr + '/elgato/accessory-info', data=json.dumps(new_data))
+        if r.status_code == requests.codes.ok:
+            self.info = new_data
+            return True
+        print(r.text)
         return False
 
     def update_color(self, on, hue, saturation, brightness):
@@ -141,3 +202,23 @@ class LightStrip:
         self.data['lights'][0]['saturation'] = saturation
         self.data['lights'][0]['brightness'] = brightness
         return self.set_strip_data(self.data)
+    def update_color_change_duration(self, duration):
+        print("old setting:", self.settings['colorChangeDurationMs'])
+        self.settings['colorChangeDurationMs'] = duration
+        return self.set_strip_info(self.settings)
+
+
+class Room:
+    """
+        Collection of lights that are on the same network
+
+    """
+    def __init__():
+        self.lights = []
+
+    def setup(service_type='_elg._tcp.local.', timeout=5):
+        self.lights = LightStrip.find_light_strips_zeroconf(service_type, timeout)
+
+    def room_color(self, on, hue, saturation, brightness):
+        for light in self.lights:
+            light.update_color(on, hue, saturation, brightness)
--- send-requests.py
+++ send-requests.py
@@ -6,18 +6,31 @@ def main():
 
     """
     lightstrips = LightStrip.find_light_strips_zeroconf()
+
+
+    print(lightstrips[0].get_strip_data())
+    print(lightstrips[1].get_strip_data())
+
+
+
+    return
+    color_change_duration = 100
+    lightstrips = LightStrip.find_light_strips_zeroconf()
     for light in lightstrips:
         data = light.get_strip_data()
-        print(data)
+        print(light.settings)
+        if not light.update_color_change_duration(color_change_duration):
+            return
     print("making the lights go in a rainbow then return to its original color")
-    original_data = [light.get_strip_data() for light in lightstrips]
-    for hue in range(0, 256):
-        sleep(0.01)
+    original_data = [light.get_strip_color() for light in lightstrips]
+    for hue in range(0, 256, 10):
+        sleep(color_change_duration / 1000)
         for light in lightstrips:
             light.update_color(1, hue, 100, 100)
 
     for i in range(len(lightstrips)):
-        lightstrips[i].set_strip_data(original_data[i])
+        on, hue, sat, bri = original_data[i]
+        lightstrips[i].update_color(on, hue, sat, bri)
 
 
 if __name__ == "__main__":
