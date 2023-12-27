9b1ea40738f843ee1ca21c96457fd805e01904ce
Blake <blakecaven@outlook.com>
2023-12-27 11:19:42 -0500

    built better logging

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Automated controller for lights
+Automated controller for lights.
 
 Timers are stored in the file pointed to by LIGHT_TIMERS
 
@@ -13,13 +13,25 @@ from time import sleep
 import sys
 import subprocess
 
-TIMER_FILE="light.transition" #"light_timers.csv"
+TIMER_FILE = "light.transition"  # "light_timers.csv"
 
+
+def usage(status):
+    """Output a help statement for the program."""
+    print("""
+Elgato Light Controller
+    USAGE python3 controller.py [FLAGS]
+
+    -l LOG_FILE     change location of log file
+    -q              turn off logging
+
+    """)
+    sys.exit(status)
 
 
 def get_timers(timer_file):
     """
-        Return a list of timers
+    Return a list of timers.
 
         Timers read from LIGHT_TIMERS file
         Raw file should read:
@@ -65,71 +77,102 @@ def get_timers(timer_file):
                 except Exception:
                     print("failed to parse scene element:", scene_element)
                     return timers
-            timers.append((time, scene_elements, False, lights)) # time to activate, elements, bool if the timer has been activated today, lights to be activated
+            # time to activate, elements, bool if the timer has been activated today, lights to be activated
+            timers.append((time, scene_elements, False, lights))
     return timers
 
+
+def check_file(file: str, old_hash: str, MODE="quiet", output_file: str = "stdout") -> bool:
+    """Check if a file changed."""
+    new_hash = subprocess.run(
+        ['md5sum', file.encode('utf-8')],
+        stdout=subprocess.PIPE).stdout.decode('utf-8')
+    if old_hash != new_hash:
+        log(
+            "checking for timers because timer file got modified",
+            MODE,
+            output_file)
+    return new_hash
+
+
+def log(message, MODE="quiet", output_file: str = "stdout"):
+    """Log the message in the appropriate place."""
+    if MODE != "quiet":
+        if MODE or output_file == "stdout":
+            print(message)
+        else:
+            out = open(output_file, 'a')
+            out.write(
+                message)
+            out.close()
+
+
 def main():
-    """
-        Main driver for program
+    """Run the program."""
+    LOG_FILE = "controller-log.log"
+    MODE = "normal"
+    # TODO: make this program respond to command line args
+    args = sys.argv[1:]
+    while args:
+        current_arg = args.pop(0)
+        if current_arg == "-l":
+            try:
+                LOG_FILE = args.pop(0)
+            except Exception:
+                usage(1)
+        if current_arg == '-q':
+            MODE = "quiet"
+        else:
+            usage(1)
 
-
-    """
     # get hash
     current_hash = subprocess.run(
-        ['md5sum', TIMER_FILE], 
+        ['md5sum', TIMER_FILE],
         stdout=subprocess.PIPE).stdout.decode('utf-8')
-    
+
     timers = get_timers(TIMER_FILE)   # get all the timers
-    # TODO: sort the timers so the earliest timer is first and the latest timer is last
+    # TODO: sort the timers
     room = Room()
     if not room.setup():            # get all the lights
-        sys.exit(1)
-        
-    print(timers)
+        usage(1)
 
-    while True: # make sure the timer never stops running
+    log(",".join(timers), MODE, LOG_FILE)
+
+    while True:  # make sure the timer never stops running
         if not timers:
             # if there are no timers, we are just going to stop the program
             sys.exit(1)
         current_time = int(datetime.now().strftime('%H%M'))
         if current_time % 5 == 0:
-            print(f"{current_time} - timers: {len(timers)}")
+            log(f"{current_time} - timers: {len(timers)}\n", MODE, LOG_FILE)
             for t in timers:
                 time, transition, activated, lights = t
-                print(f"\t{time} : {'done' if activated else 'waiting'}")
-
-
+                log(f"\t{time} : {'done' if activated else 'waiting'}\n", MODE, LOG_FILE)
         for index, timer in enumerate(timers):
             time, transition, activated, lights = timer
             if abs(current_time - time) <= 1 and not activated:
-                print(f"controller ran: {transition} at {time}")
+                log(f"controller ran: {transition} at {time}\n", MODE, LOG_FILE)
                 # run the transition
                 if lights:
-                    print(f"only transitioning lights: {lights}")
+                    log(f"only transitioning lights: {lights}\n", MODE, LOG_FILE)
                     for light in lights:
                         room.light_transition(light, transition)
                 else:
-                    print("ran transition on all lights")
+                    log("ran transition on all lights\n", MODE, LOG_FILE)
                     room.room_transition(transition)
-                activated = True # set the timer to activated
+                activated = True  # set the timer to activated
             elif current_time <= 1:
                 activated = False
 
             timers[index] = (time, transition, activated, lights)
-        sleep(60) # wait a minute
+        sleep(60)  # wait a minute
 
         # check for any new timers only if the timer file has changed
-        new_hash = subprocess.run(
-            ['md5sum', TIMER_FILE.encode('utf-8')], 
-            stdout=subprocess.PIPE).stdout.decode('utf-8')
+        new_hash = check_file(TIMER_FILE, current_hash, MODE, LOG_FILE)
         if current_hash != new_hash:
-            print("checking for timers because timer file got modified")
             timers = get_timers(TIMER_FILE)
-        
-        current_hash = new_hash
+            current_hash = new_hash
         # and repeat the process
-
-
 
 
 if __name__ == "__main__":
