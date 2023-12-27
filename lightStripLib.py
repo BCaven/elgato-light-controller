7abab6f37dda299a6400f6f518b06eb9801cb954
Blake <blakecaven@outlook.com>
2023-12-27 11:19:59 -0500

    making transitions better

--- lightStripLib.py
+++ lightStripLib.py
@@ -1,92 +1,114 @@
 """
-controller for elgato light strips
+controller for elgato light strips.
 
 Reference: github.com/zunderscore/elgato-light-control/
 """
 
 import requests
 import socket
-from multiprocessing import Pool
 import json
 from time import sleep, time
 
 NUM_PORTS = 65536
 ELGATO_PORT = 9123
 
+
 class ServiceListener:
+    """Listener for Zeroconf."""
+
     def __init__(self):
+        """Init the listener."""
         self.services = []
+
     def get_services(self):
+        """Return the services."""
         return self.services
+
     def remove_service(self, zeroconf, type, name):
-        print("Service %s removed" % (name,))
+        """Unused but required for zeroconf."""
+
     def update_service(self, zeroconf, type, name):
-        pass
+        """Unusued but required for zeroconf."""
+
     def add_service(self, zeroconf, type, name):
+        """Add a service to the list."""
         info = zeroconf.get_service_info(type, name)
         self.services.append(info)
 
+
 class Scene:
     """
-        class to store and manipulate scenes at a high level
-            {
-                'numberOfLights': 1,
-                'lights': [
-                    {'on': 1,
-                     'id': 'com.corsair.cc.scene.sunrise',
-                     'name': 'Sunrise',
-                     'brightness': 100.0,
-                     'numberOfSceneElements': 4,
-                     'scene': []
-                     }
-                ]
+    Store and manipulate scenes at a high level.
+
+    {
+        'numberOfLights': 1,
+        'lights': [
+            {'on': 1,
+            'id': 'com.corsair.cc.scene.sunrise',
+            'name': 'Sunrise',
+            'brightness': 100.0,
+            'numberOfSceneElements': 4,
+            'scene': []
             }
-
+        ]
+    }
     """
-    def __init__(self, input_scene=[]):
-        """
 
-        """
+    def __init__(self, input_scene=[]):
+        """Init the scene."""
         self.data = input_scene
 
     def add_scene(self, hue, saturation, brightness, durationMs, transitionMs):
-        """
-            add an item to the end of the list
-        """
-        self.data.append({'hue': hue, 'saturation': saturation, 'brightness': brightness, 'durationMs': durationMs, 'transitionMs': transitionMs})
-    def insert_scene(self, index, hue, saturation, brightness, durationMs, transitionMs):
-        """
+        """Add an item to the end of the list."""
+        self.data.append(
+            {'hue': hue,
+             'saturation': saturation,
+             'brightness': brightness,
+             'durationMs': durationMs,
+             'transitionMs': transitionMs})
 
-        """
-        self.data.insert(index, {'hue': hue, 'saturation': saturation, 'brightness': brightness, 'durationMs': durationMs, 'transitionMs': transitionMs})
+    def insert_scene(self,
+                     index,
+                     hue,
+                     saturation,
+                     brightness,
+                     durationMs,
+                     transitionMs):
+        """Insert a scene in the list."""
+        self.data.insert(
+            index,
+            {'hue': hue,
+             'saturation': saturation,
+             'brightness': brightness,
+             'durationMs': durationMs,
+             'transitionMs': transitionMs})
 
     def delete_scene(self, index=0):
+        """Remove a scene from the list."""
         return self.data.pop(index)
 
     def print_scenes(self):
-        """
-            Display every scene in the loop
-        """
-        #print(self.data['scene'])
+        """Display every scene in the loop."""
         for scene in self.data:
             print(scene)
 
     def length(self):
+        """Return the duration of the scene."""
         scene_length = 0
         for scene in self.data['scene']:
             scene_length += scene['durationMs']
             scene_length += scene['transitionMs']
         return scene_length
 
+
 def save_timer_to_file(file: str, time: str, lights: list, scene: list):
     """
-        Save a timer in file so the controller can read it in
+    Save a timer in file so the controller can read it in.
 
         Format: TIME, LIGHT|LIGHT|etc, SCENE, SCENE, SCENE, etc
         TIME    = HHMM
         LIGHT   = ip addr : port
         SCENE   = HUE|SATURATION|BRIGHTNESS|DURATION_MS|TRANSITION_MS
-
     """
     with open(file, 'a') as output_file:
         output_list = [time, "|".join(lights)]
@@ -98,8 +120,10 @@ def save_timer_to_file(file: str, time: str, lights: list, scene: list):
 
 class LightStrip:
     """
-        LightStrip language
-        data: json object,  can be retrieved by making a get request to light.full_addr/elgato/lights
+    LightStrip language.
+
+    data: json object,  can be retrieved by making a get request to
+    light.full_addr/elgato/lights
             always has two keys:
                 'numberOfLights': int
                 'lights'        : list
@@ -107,7 +131,8 @@ class LightStrip:
                 'on'            : int
                 'brightness'    : int
             however, the lights have two (known) modes: individual colors, and scenes
-                for individual colors, the dictionary additionally has 'hue' and 'saturation' keys (both are of type `float`)
+                for individual colors, the dictionary additionally has 'hue' and
+                'saturation' keys (both are of type `float`)
                 for scenes:
                     'id'                        : str
                     'name'                      : str
@@ -121,13 +146,14 @@ class LightStrip:
                     'transitionMs'              : int
             when there is a 'scene', the light loops through each item in the scene
     """
+
     def __init__(self, addr, port, name=""):
+        """Initialize the light."""
         self.addr = addr
         self.port = port
         self.name = name
         self.full_addr = self.addr + ':' + str(self.port)
-        #print("full addr", self.full_addr)
-        self.get_strip_data() # fill in the data/info/settings of the light
+        self.get_strip_data()  # fill in the data/info/settings of the light
         self.get_strip_info()
         self.get_strip_settings()
         self.is_scene = False
@@ -137,12 +163,14 @@ class LightStrip:
 
     def find_light_strips_zeroconf(service_type='_elg._tcp.local.', TIMEOUT=5):
         """
-            Use multicast to find all elgato light strips
+        Use multicast to find all elgato light strips.
+
             Parameters:
                 the service type to search
                 the timeout period to wait until you stop searching
         """
-        # NOTE: need to put this in a try/except statement just in case they have not imported zeroconf
+        # NOTE: need to put this in a try/except statement
+        # just in case they have not imported zeroconf
         try:
             import zeroconf
         except Exception:
@@ -152,7 +180,6 @@ class LightStrip:
 
         lightstrips = []
 
-        #print("attempting to find everything on", service_type)
         zc = zeroconf.Zeroconf()
         listener = ServiceListener()
         browser = zeroconf.ServiceBrowser(zc, service_type, listener)
@@ -169,8 +196,8 @@ class LightStrip:
 
         return lightstrips
 
-
     def find_light_strips_manual(strips) -> list:
+        """TODO: write this method."""
         # oof we have to do it the hardcore way or just give up...
         lightstrips = []
         for (addr, port) in strips:
@@ -188,83 +215,104 @@ class LightStrip:
                 return port
             else:
                 return 0
-                #print("port:", i, "was closed")
         except Exception:
             pass
 
-    def check_all_ports(self, num_ports):
+    def __check_all_ports(self, num_ports):
         for port in range(num_ports):
             yield LightStrip.__is_socket_open((self.addr, self.port))
 
     def get_strip_data(self):
         """
-            Send a get request to the full addr
+        Send a get request to the full addr.
+
             format:
             http://<IP>:<port>/elgato/lights
         """
-        self.data = requests.get('http://' + self.full_addr + '/elgato/lights', verify=False).json()
+        self.data = requests.get(
+            'http://' + self.full_addr + '/elgato/lights',
+            verify=False).json()
         return self.data
 
     def get_strip_info(self):
         """
-            Send a get request to the light
+        Send a get request to the light.
+
             should get back something in this format:
         """
-        self.info = requests.get('http://' + self.full_addr + '/elgato/accessory-info', verify=False).json()
+        self.info = requests.get(
+            'http://' + self.full_addr + '/elgato/accessory-info',
+            verify=False).json()
         return self.info
 
     def get_strip_settings(self):
-        """
-
-        """
-        self.settings = requests.get('http://' + self.full_addr + '/elgato/lights/settings', verify=False).json()
+        """Get the strip's settings."""
+        self.settings = requests.get(
+            'http://' + self.full_addr + '/elgato/lights/settings',
+            verify=False).json()
         return self.settings
+
     def get_strip_color(self):
         """
-            Return the color of the light
-            If the light is not set to a specific color (i.e. when it is in a scene) then the tuple is empty
+        Return the color of the light.
+
+            If the light is not set to a specific color
+            (i.e. when it is in a scene) then the tuple is empty
         """
         try:
             light_color = self.get_strip_data()['lights'][0]
-            return (light_color['on'], light_color['hue'], light_color['saturation'], light_color['brightness'])
+            return (
+                light_color['on'],
+                light_color['hue'],
+                light_color['saturation'],
+                light_color['brightness'])
         except Exception:
-            return () # the light strip is not set to a static color
+            return ()  # the light strip is not set to a static color
 
     def set_strip_data(self, new_data: json) -> bool:
         """
-            Sends a put request to update the light data
-            Returns True if successful
+        Send a put request to update the light data.
+
+        Returns True if successful
         """
-        r = requests.put('http://' + self.full_addr + '/elgato/lights', data=json.dumps(new_data))
+        r = requests.put(
+            'http://' + self.full_addr + '/elgato/lights',
+            data=json.dumps(new_data))
         # if the request was accepted, modify self.data
         if r.status_code == requests.codes.ok:
             self.data = r.json()
             return True
         print(r.text)
         return False
+
     def set_strip_settings(self, new_data: json) -> bool:
         """
-            Send a put request to update the light settings
-            Returns True on success
+        Send a put request to update the light settings.
+
+        Returns True on success
         """
-        r = requests.put('http://' + self.full_addr + '/elgato/lights/settings', data=json.dumps(new_data))
+        r = requests.put(
+            'http://' + self.full_addr + '/elgato/lights/settings',
+            data=json.dumps(new_data))
         print(r.text)
         if r.status_code == requests.codes.ok:
             self.settings = new_data
             return True
         return False
+
     def set_strip_info(self, new_data: json) -> bool:
-        r = requests.put('http://' + self.full_addr + '/elgato/accessory-info', data=json.dumps(new_data))
+        """Set the strip info."""
+        r = requests.put(
+            'http://' + self.full_addr + '/elgato/accessory-info',
+            data=json.dumps(new_data))
         if r.status_code == requests.codes.ok:
             self.info = new_data
             return True
         print(r.text)
         return False
 
-    def update_color(self, on, hue, saturation, brightness):
-        """
-            User friendly way to interact with json data to change the color
-        """
+    def update_color(self, on, hue, saturation, brightness) -> bool:
+        """User friendly way to interact with json data to change the color."""
         self.data = {
             'numberOfLights': 1,
             'lights': [
@@ -277,10 +325,15 @@ class LightStrip:
         return self.set_strip_data(self.data)
 
     def update_scene_data(self, scene):
+        """Update just the scene data."""
         self.data['lights'][0]['scene'] = scene.data
         self.data['lights'][0]['numberOfSceneElements'] = len(scene.data)
 
-    def make_scene(self, name: str, scene_id: str, brightness:float = 100.0):
+    def make_scene(self,
+                   name: str,
+                   scene_id: str,
+                   brightness: float = 100.0):
+        """Create a scene."""
         self.data = {
             'numberOfLights': 1,
             'lights': [
@@ -294,70 +347,92 @@ class LightStrip:
             ]
         }
         self.is_scene = True
-        self.scene = Scene([]) # if you do not specify an empty scene, it might copy old scene data... annoying
+        # if you do not specify an empty scene,
+        # it might copy old scene data... annoying
+        self.scene = Scene([])
 
-    def transition_start(self, colors: list, name='transition-scene', scene_id='transition-scene-id') -> int:
+    def transition_start(self,
+                         colors: list,
+                         name='transition-scene',
+                         scene_id='transition-scene-id') -> tuple[int, bool]:
         """
-            Non-blocking for running multiple scenes
-            returns how long to wait
-            
+        Non-blocking for running multiple scenes.
+
+        returns how long to wait
         """
         self.make_scene(name, scene_id, 100)
         wait_time_ms = 0
-        # check if the light has already been set to a color, and if it has, make that color the start of the transition scene
+        # check if the light has already been set to a color,
+        # and if it has, make that color the start of the transition scene
         if current_color := self.get_strip_color():
             _, hue, saturation, brightness = current_color
-            self.scene.add_scene(hue, saturation, brightness, colors[0][3], colors[0][4])
+            self.scene.add_scene(
+                hue,
+                saturation,
+                brightness,
+                colors[0][3],
+                colors[0][4])
             wait_time_ms += colors[0][4] + colors[0][3]
         # add the colors in the new scene
         for color in colors:
             hue, saturation, brightness, durationMs, transitionMs = color
-            #print(hue, saturation, brightness, durationMs, transitionMs)
-            self.scene.add_scene(hue, saturation, brightness, durationMs, transitionMs)
-            wait_time_ms += durationMs + transitionMs        
-        
+            self.scene.add_scene(
+                hue,
+                saturation,
+                brightness,
+                durationMs,
+                transitionMs)
+            wait_time_ms += durationMs + transitionMs
         # update the light with the new scene
         self.update_scene_data(self.scene)
-        self.set_strip_data(self.data)
-        #print("new data:")
-        #print(self.data)
+        light_status = self.set_strip_data(self.data)
         # return the wait time
-        return (wait_time_ms - colors[-1][3] - colors[-1][4]) / 1000
+        return ((wait_time_ms - colors[-1][3] - colors[-1][4]) / 1000,
+                light_status)
 
-    def transition_end(self, end_color: tuple[float, float, float]):
+    def transition_end(self, end_color: tuple[float, float, float]) -> bool:
         """
-            used after transition_start is called
-            sets the scene to the end_color
-            almost identical to lightStrip.update_color - primarily used to keep code readable
+        Use after transition_start is called.
+
+        sets the scene to the end_color
+        almost identical to lightStrip.update_color
+        primarily used to keep code readable
         """
         is_on = 1 if end_color[2] > 0 else 0
-        self.update_color(is_on, end_color[0], end_color[1], end_color[2])
+        return self.update_color(
+            is_on,
+            end_color[0],
+            end_color[1],
+            end_color[2])
+
 
 class Room:
-    """
-        Collection of lights that are on the same network
+    """Collection of lights that are on the same network."""
 
-    """
-    def __init__(self, lights:list=[]):
+    def __init__(self, lights: list = []):
+        """Init the room."""
         self.lights = lights
 
     def setup(self, service_type='_elg._tcp.local.', timeout=5):
-        self.lights = LightStrip.find_light_strips_zeroconf(service_type, timeout)
+        """Find all the lights."""
+        self.lights = LightStrip.find_light_strips_zeroconf(
+            service_type, timeout)
         return True if self.lights else False
+
     def room_color(self, on, hue, saturation, brightness):
+        """Set color for the whole room."""
         for light in self.lights:
             light.update_color(on, hue, saturation, brightness)
 
     def room_scene(self, scene):
-        """
-            Set all lights in the room to a specific scene
+        """Set all lights in the room to a specific scene."""
+        # TODO: write this method
 
-        """
-
-    def room_transition(self, colors:list, name='transition-scene', scene_id='transition-scene-id'):
-        """
-            Non blocking transition for all room lights
-        """
+    def room_transition(self,
+                        colors: list,
+                        name='transition-scene',
+                        scene_id='transition-scene-id'):
+        """Non blocking transition for all room lights."""
         times = []
         for light in self.lights:
             t = light.transition_start(colors, name, scene_id)
@@ -370,14 +445,19 @@ class Room:
             else:
                 times.append((light, sleep_time, start_time))
 
-    def light_transition(self, addr:str, colors:list, name='transition-scene', scene_id='transition-scene-id'):
-        """
-            Non blocking transition for specific light in the room
-        """
+    def light_transition(self,
+                         addr: str,
+                         colors: list,
+                         name='transition-scene',
+                         scene_id='transition-scene-id'):
+        """Non blocking transition for specific light in the room."""
         times = []
         for light in self.lights:
             if light.addr == addr:
-                times.append((light, light.transition_start(colors, name, scene_id), time()))
+                times.append((
+                    light,
+                    light.transition_start(colors, name, scene_id),
+                    time()))
 
         while times:
             light, sleep_time, start_time = times.pop(0)
