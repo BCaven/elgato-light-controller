56e0be1df55f9725d29e2297c39f6dd773d29fcc
BCaven <blakecaven@outlook.com>
2024-03-12 17:56:59 -0400

    working parser - now just need to add rules

--- parse_rules.py
+++ parse_rules.py
@@ -1,63 +1,16 @@
 """
-parse_rules.py
+parse_re.py
 
-Rules follow the language
-
-S -> (S)
-S -> S | S
-S -> MONTH
-S -> DAY
-S -> YEAR
-S -> HOUR
-S -> MINUTE
-MINUTE -> RANGE(MINUTE, MINUTE)
-DAY -> RANGE(DAY, DAY)
-YEAR -> RANGE(YEAR, YEAR)
-MONTH -> RANGE(MONTH, MONTH)
-
-RANGE is just union of several items of the same raw type
-
-MONTH,DAY,YEAR,MIN,HOUR nodes all have same type
-theu can get converted to nodes of type RorN
-Range nodes are of type RorN
-Unions can only happen between nodes of type RorN
-RorN nodes can get converted to same type as union
-
-parens can only close nodes of type union
-
-RULES:
-below       |   pop     |   read    |   next    |   push    |
-# turning input into nodes
-$,NODE,(,|  |   &       |   Mint    |           |MONTH(int) |
-$,NODE,(,|  |   &       |   dint    |           | DAY(int)  |
-$,NODE,(,|  |   &       |   yint    |           | YEAR(int) |
-$,NODE,(,|  |   &       |   mint    |           | MIN(int)  |
-$,NODE,(,|  |   &       |   hint    |           | HOUR(int) |
-# turning ranges into range nodes
-INT         |   &       |   -       |           |   -
-$,(,NODE    |MONTH-MONTH|   &       |           | RANGE(a,b)| # this rule is repeated for every node type
-
-# unions RorN => type is range or finished int node
-$ (         |FINAL FINAL|    &      |           | UNION(a,b)|
-
-# paren rules
-            | (finished)|   &       |           | RorN(a)
-any         |   &       |   (       |           | (
-union       |   &       |   )       |           | ) # might want to combine this with the first paren rule
-
-
+CP 2 Part 1
+Parse a regular expression and return a syntax tree
 """
-#from cp1.nfa import NFA
 import sys
 
 # rules structured as below, pop, read, next, push
 # below and next are sets, pop, read, and push are strings
 # empty next sets mean anything can be next
-# TODO: find better way to represent functions
-# TODO: sub symbol for '⊣' <- that is the terminal symbol (end of string)
-# TODO: figure out what this means: U \ {*}
 class Node:
-    def __init__(self, operation: str = "", cfg_type: str = "NONE", parameters = []):
+    def __init__(self, operation: str = "NONE", cfg_type: str = "NONE", parameters = []):
         # if these matches dont work, will need to add the CFG symbol that goes with the state
         assert type(operation) is str, f"invalid construction, operation is not a string: {operation}"
         self.operation = operation
@@ -65,9 +18,20 @@ class Node:
         self.parameters = parameters
         assert type(cfg_type) is str, f"invalid construction, cfg_type is not a string"
         self.cfg_type = cfg_type
+    
+    def __str__(self):
+        params = []
+        for p in self.parameters:
+            if type(p) is Node:
+                params.append(str(p))
+            else:
+                if p != "&":
+                    params.append('"' + str(p) + '"')
+        return f"{self.operation}({','.join(params)})"
+    
 
     def __repr__(self):
-        return f"{self.operation}{self.cfg_type}({",".join(str(p) for p in self.parameters)})"
+        return f"{self.operation}{self.cfg_type}({','.join(str(p) for p in self.parameters)})"
     
     def __eq__(self, other):
         if type(other) == type(self):
@@ -82,147 +46,202 @@ class Node:
     def __hash__(self):
         return hash(str(type(self))+self.cfg_type)
 
-# TODO: see if I can cut out some rules
 # if the popped item is a str, just treat it as normal
 # if the popped item is a Node, just make sure the popped type is also a Node
 # in these rules, 'a' is any char that isnt a reserved symbol
-RESERVED_SYMBOLS = {'(', ')', '|', '*'}
-# TODO redo RULES to incorporate typing used by chiang
+RESERVED_SYMBOLS = {'(', ')', '|', '*', '⊣', '$'}
 RULES = [
     # rules for reading input
-    [{'$', '(', '|', Node()}, ['&'], 'a', set(), Node('symbol')], # a, & -> a for a in E
-    [{'$', '(', '|', Node()}, ['&'], '(', set(), '('], # (, & -> (
-    [{Node()}, ['&'], ')', set(), ')'], # ), & -> )
-    [{Node()}, ['&'], '|', set(), '|'], # |, & -> |
-    [{Node()}, ['&'], '*', set(), '*'], # *, & -> *
+    [{'$', '(', '|', Node(cfg_type='T')}, ['&'], 'a', set(), 'a'], # a, & -> a for a in E
+    [{'$', '(', '|', Node(cfg_type='T')}, ['&'], '(', set(), '('], # (, & -> (
+    [{Node(cfg_type='E')}, ['&'], ')', set(), ')'], # ), & -> )
+    [{Node(cfg_type='E')}, ['&'], '|', set(), '|'], # |, & -> |
+#    [{Node(cfg_type='P')}, ['&'], '*', set(), '*'], # *, & -> *
     # rules for converting input to nodes
-    #[{}, ['Ea', '|', 'Mb'], '&', {}, 'Eunion(a, b)'], # ε, E|M → E
-    [set(), [Node(), '|', Node()], '&', set(), Node('union')], # ε, E|M → E
-
-    # I think this is unnecessary
-    #[{'$', '('}, ['Ma'], '&', {}, 'Ea'], # ε, M → E
-
-    #[{'$', '(', '|'}, ['&'], '&', {'|', '(', '⊣'}, 'Mepsilon()'], # ε, ε → M
-    [{'$', '(', '|'}, ['&'], '&', {'|', ')', '⊣'}, Node('epsilon')], # ε, ε → M
-
-    # I think this one is also unnecessary
-    #[{}, ['Ta'], '&', {'|', ')', '⊣'}, 'Ma'], # ε,T → M
-
-    #[{}, ['Ta', 'Fb'], '&', {}, 'Tconcat(a, b)'], # ε,TF → T
-    [set(), [Node(), Node()], '&', set(), Node('concat')], # ε,TF → T
-
-    # this one also might not be needed
-    #[{'$', '(', '|'}, ['Fa'], '&', {}, 'Ta'], # ε, F → T
-
-    #[{}, ['Pa', '*'], '&', {}, 'Fstar(a)'], # ε, P* → F
-    [set(), [Node(), '*'], '&', set(), Node('star')], # ε, P* → F
-
-    # I think this is supposed to be the terminal character, but will have to check
-    # might not be necessary
-    #[{}, ['Pa'], '&', {'U \ {*}'}, 'Fa'], # ε, P → F
-
-    #[{}, ['a'], '&', {}, 'Psymbol(a)'], # ε, a → P for a ∈ Σ
-    [set(), ['a'], '&', set(), Node('symbol')], # ε, a → P for a ∈ Σ
-
+    [set(), [Node(cfg_type='E'), '|', Node(cfg_type='M')], '&', set(), Node('union', cfg_type='E')], # ε, E|M → E
+    [{'$', '('}, [Node(cfg_type='M')], '&', set(), Node(cfg_type='E')], # ε, M → E
+    [{'$', '(', '|'}, ['&'], '&', {'|', ')', '⊣'}, Node('epsilon', cfg_type='M')], # ε, ε → M
+    [set(), [Node(cfg_type='T')], '&', {'|', ')', '⊣'}, Node(cfg_type='M')], # ε,T → M
+    [set(), [Node(cfg_type='T'), Node(cfg_type='F')], '&', set(), Node('concat', cfg_type='T')], # ε,TF → T
+    [{'$', '(', '|'}, [Node(cfg_type='F')], '&', set(), Node(cfg_type='T')], # ε, F → T
+#    [set(), [Node(cfg_type='P'), '*'], '&', set(), Node('star', cfg_type='F')], # ε, P* → F
+    # next character cannot be a star
+    [set(), [Node(cfg_type='P')], '&', {'NOT_STAR'}, Node(cfg_type='F')], # ε, P → F
+    [set(), ['a'], '&', set(), Node('symbol', cfg_type='P')], # ε, a → P for a ∈ Σ
     # this just removes parenthesese that only have one item in them
-    #[{}, ['(', 'Ea', ')'], '&', {}, 'Pa'], # ε, (E) → P
-    [set(), ['(', Node(), ')'], '&', set(), Node('input')], # ε, (E) → P
-
+    [set(), ['(', Node(cfg_type='E'), ')'], '&', set(), Node(cfg_type='P')], # ε, (E) → P
 ]
+
+def check_rule(rule, stack, input_str) -> bool:
+    """
+    Separate function to check rules so the main parser loop is less cluttered
+    Returns a boolean if the rule is allowed
+
+    the token below whatever is getting popped must be in the set {below}
+    every item in [pop] must be at the top of the stack in the reverse order they are presented (last first)
+    the value read from the input string must be equal to *read*. the character 'a' means any character that is not a RESERVED_SYMBOL
+    and '&' means nothing was read from the input string
+    the value after read in the input string must be found in the set {next_token}
+    """
+    below, pop_tokens, read, next_tokens, push_token = rule
+    # make sure the rules are well formed
+    assert type(below) is set, f"failed to read below from rule {rule}"
+    assert type(pop_tokens) is list, f"failed to read pop from rule {rule}"
+    assert type(read) is str, f"failed to read 'read' from rule {rule}"
+    assert type(next_tokens) is set, f"failed to read next_token from rule {rule}. Recieved token: {next_tokens}"
+    assert type(push_token) is str or Node, f"failed to read push from rule {rule}. Recieved token: {push_token}"
+    
+    # check below tokens
+    below_index = (len(pop_tokens) + 1)  * -1 if pop_tokens != ['&'] else -1
+    if below:
+        if len(stack) <= (below_index * -1) -1:
+            return False
+        if stack[below_index] not in below:
+            return False
+        
+    
+    # check pop items
+    if pop_tokens != ['&']:
+        stack_pop_items = stack[below_index + 1:]
+        if pop_tokens == ['a']:
+            if len(stack_pop_items) != 1:
+                return False
+            if stack_pop_items[0] in RESERVED_SYMBOLS:
+                return False
+            if type(stack_pop_items[0]) is Node:
+                return False
+        elif stack_pop_items != pop_tokens:
+            return False
+    
+    # check read value
+    # all node rules do not read from the input string
+    # if we arent supposed to read anything then dont worry too much about it
+    if read != '&':
+        # if we are supposed to read something but there is nothing there, obviously the check fails
+        if len(input_str) == 0:
+            return False
+        if read == 'a':
+            # any char that isnt reserved
+            if input_str[0] in RESERVED_SYMBOLS:
+                return False
+            # or a node
+            if type(input_str[0]) is Node:
+                return False
+        # if its a reserved symbol, make sure we are reading in the right one
+        elif input_str[0] != read:
+                return False
+    
+    # make sure the next thing in the input is in next_tokens
+    if len(next_tokens) != 0:
+        next_index = 0 if read == '&' else 1
+        if len(input_str) > next_index:
+            if 'NOT_STAR' in next_tokens:
+                # special case
+                if input_str[next_index] == '*':
+                    return False
+            elif input_str[next_index] not in next_tokens:
+                return False
+        else:
+            if 'NOT_STAR' not in next_tokens:
+                return False
+    # none of the checks failed so the rule applies
+    return True
 
 def parser(regex: str):
     """
     basic approach:
         List of transitions are all possible transitions.
         For every char, find the transition that matches the input/stack 
-    
-    TODO: fix union
-    TODO: fix star
-    
+   
     """
-    # TODO: add 'end of string character'
-    remaining_input = list(regex)
+    remaining_input = ['⊣']
+    if len(regex) > 0:
+        remaining_input = list(regex + '⊣') if regex[-1] != '⊣' else list(regex)
     stack = ['$']
     # when the stack does not change, it means we have an invalid regex (because we should always follow exactly one rule)
     a_rule_was_followed = True
 
     while a_rule_was_followed:
-        print(f"remaining input: {remaining_input}")
-        print(f"stack: {stack}")
-        next_input = remaining_input.pop(0) if len(remaining_input) > 0 else "&"
+        #print(f"stack: {stack}")
         a_rule_was_followed = False
-        for rule in RULES:
-            # basic idea is to make sure the rule is valid, and if it is - to do do it
-            # if it isnt valid, just skip that rule
-            invalid = False
-            below, pop, read, next_token, push = rule
-            # make sure the rules are well formed
-            assert type(below) is set, f"failed to read below from rule {rule}"
-            assert type(pop) is list, f"failed to read pop from rule {rule}"
-            assert type(read) is str, f"failed to read 'read' from rule {rule}"
-            assert type(next_token) is set, f"failed to read next_token from rule {rule}. Recieved token: {next_token}"
-            assert type(push) is str or Node, f"failed to read push from rule {rule}. Recieved token: {push}"
-            print(f"checking rule: {rule}")
-            # if the top of the stack is not in 'below' and 'below' exists then the rule is invalid
-            if stack[-1] not in below and below:
-                print(f"top of stack is not in below.\ntop of stack: {stack[-1]} not in {below}")
-                invalid = True
-            # if we pop nothing then its fine
-            if pop != ['&']:
-                # make sure the stack has what is about to be popped
-                for index, item in enumerate(pop):
-                    if stack[-1 - (len(pop) - index - 1)] != item:
-                        print(f"item {item} was not next in the stack - comparison: {stack[-1 - (len(pop) - index -1)]}")
-                        invalid = True
-                        break
+        for rule in RULES:    
+            """
+            Checking rules:
+                the token below whatever is getting popped must be in the set {below}
+                every item in [pop] must be at the top of the stack in the reverse order they are presented (last first)
+                the value read from the input string must be equal to *read*. the character 'a' means any character that is not a RESERVED_SYMBOL
+                and '&' means nothing was read from the input string
+                the value after read in the input string must be found in the set {next_token}
+
+                if all of those rules are met, 
+                every item in pop is popped from the stack
+                the value 'read' is read from input
+                the value 'push' is pushed to the top of the stack
+                    if 'push' is a node:
+                        the new node gets the cfg_type of the 'push' node
+                        if it does not have an 'operation' then give it the operation of the node that got popped
+                            also give it the parameters of the node that got popped
+                        if it does have an operation, its parameters are the items that got popped from the stack
+                    if 'push' is not a node:
+                        push the 'read' value that was obtained from the input
                 
-            if next_input != read and read != '&' and read != 'a':
-                print(f"wrong read: {next_input} != {read}")
-                invalid = True
-
-            if read == 'a':
-                if next_input in RESERVED_SYMBOLS or next_input == '&':
-                    print(f"wrong read value - {read} != {next_input}")
-                    invalid = True
-            if len(remaining_input) > 0 and len(next_token) > 0:
-                if next_token and remaining_input[0] not in next_token:
-                    print(f"invalid next token: {remaining_input[0]} not in {next_token}")
-                    invalid = True
-
-            if invalid:
+                if no rule was met, we are done.
+            """
+            if not check_rule(rule, stack, remaining_input):
                 continue
+            
+            _, pop_tokens, read, _, push_token = rule
             # now all of the conditions should be met, so we can perform the operation
             # pop the old items
-            print(f"following rule: {rule}")
+
             popped_from_stack = []
-            for item in pop:
-                if item == "&":
-                    continue
-                popped_item = stack.pop()
-                if popped_item not in RESERVED_SYMBOLS:
-                    popped_from_stack.append(popped_item)
+            if pop_tokens != ['&']:
+                for token in reversed(pop_tokens):
+                    popped_token = stack.pop()
+                    if pop_tokens != ['a']:
+                        assert token == popped_token, f"when popping from stack tokens did not match: {token} != {popped_token}"
+                    else:
+                        assert token not in RESERVED_SYMBOLS, f"was supposed to pop arbitrary character but instead popped {token}"
+                        assert type(token) != Node, f"was supposed to pop arbitrary character but instead popped {token}"
+                    if popped_token not in RESERVED_SYMBOLS:
+                        popped_from_stack.insert(0, popped_token)
+                assert pop_tokens == ['a'] or popped_from_stack == list(filter(lambda x: x not in RESERVED_SYMBOLS, pop_tokens)), f"popped tokens do not match: {popped_from_stack} != {list(filter(lambda x: x not in RESERVED_SYMBOLS, pop_tokens))}"
+            
+            popped_from_input = ""
+            if read != '&':
+                assert len(remaining_input) > 0, "tried to pop input from empty list"
+                popped_from_input = remaining_input.pop(0)
+            
+            if type(push_token) is Node:
+                assert push_token.cfg_type != 'NONE', f"malformed node: {push_token}"
+                new_node = Node(cfg_type=push_token.cfg_type)
+                if push_token.operation == 'NONE':
+                    assert len(popped_from_stack) == 1, f"only one item was supposed to be popped but found a different number: {popped_from_stack}"
+                    assert type(popped_from_stack[0]) is Node, f"was supposed to pop a node but instead popped: {popped_from_stack[0]}"
+                    assert popped_from_stack[0].operation != 'NONE', f"popped node was supposed to have an operation but did not: {popped_from_stack[0]}"
+                    new_node.operation = popped_from_stack[0].operation
+                    new_node.parameters = popped_from_stack[0].parameters
                 else:
-                    # ignored reserved symbol
-                    pass
-                
-            pushed_item = push
-            # todo: make this work for functions
-            if push == Node():
-                # If we are making a symbol node, the parameter should be the token we read in, since nothing should have been popped from the stack
-                if push.operation == 'symbol':
-                    assert len(popped_from_stack) == 0, f"bad parsing, was supposed to push a symbol, but popped values from the stack: {popped_from_stack}"
-                    popped_from_stack = [next_input]
-                pushed_item = Node(push.operation, parameters=popped_from_stack)
-            stack.append(pushed_item)
-
-
+                    new_node.operation = push_token.operation
+                    if push_token.operation == 'epsilon':
+                        new_node.parameters = ['&']
+                    else:
+                        new_node.parameters = popped_from_stack
+                assert new_node.operation != 'NONE', f"trying to push malformed node to stack - no operation"
+                assert new_node.parameters != [], f"trying to push malformed node to stack - no parameters"
+                stack.append(new_node)
+            else:
+                assert popped_from_input != "", f"was supposed to push the value from input but nothing was popped from input"
+                stack.append(popped_from_input)
 
             a_rule_was_followed = True
             break
-        # if we are at the end of the input and there is only one token left in the stack (it has to be a Node) then return that node
-        if len(remaining_input) == 0 and len(stack) == 2:
-            assert type(stack[-1]) is Node, f"whoops, the last thing on the stack was not a Node, instead it was: {stack}"
-            return stack.pop()
+        if not a_rule_was_followed:
+            # if we are at the end of the input and there is only one token left in the stack (it has to be a Node) then return that node
+            if len(stack) == 2:
+                assert type(stack[-1]) is Node, f"whoops, the last thing on the stack was not a Node, instead it was: {stack}"
+                return stack.pop()
+        
         assert a_rule_was_followed, "this is unnecessary, but whatever, the loop will break anyways if a rule isnt performed"
 
 def main():
